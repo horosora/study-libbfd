39e3c144574f7e5ff1b4a31c5bbf3134144bbcb2
horosora <horosora@gmail.com>
2022-02-13 22:42:00 +0900

    Get the architecture

---
+++ Makefile
@@ -0,0 +1,3 @@
+.PHONY: build
+build:
+	gcc binary_analysis.c -o binary_analysis -lbfd
---
+++ binary_analysis.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <bfd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("%s [binary]\n", argv[0]);
+        return 0;
+    }
+
+    bfd_init();
+
+    bfd *file = bfd_openr(argv[1], NULL);
+
+    if (!file) {
+        puts("Failed to read the binary");
+        return 1;
+    }
+
+    if (!bfd_check_format(file, bfd_object)) {
+        puts("Not an executable file");
+        return 1;
+    }
+
+    if (bfd_get_flavour(file) == bfd_target_unknown_flavour) {
+        puts("Unknown format");
+        return 1;
+    }
+
+    printf("arch: %s\n", bfd_get_arch_info(file)->printable_name);
+
+    return 0;
+}
